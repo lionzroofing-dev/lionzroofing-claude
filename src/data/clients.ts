6d0327c34774480561ae0ad165cbc25cc4419093
jazur <azurjonathan@gmail.com>
2026-05-28 00:33:34 +0800

    update logo

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -4,18 +4,18 @@ export interface Client {
 }
 
 export const clients: Client[] = [
-  { name: "Client 1",  logo: "/images/clients/logo-lionz-01.png" },
-  { name: "Client 2",  logo: "/images/clients/logo-lionz-02.png" },
-  { name: "Client 3",  logo: "/images/clients/logo-lionz-03.png" },
-  { name: "Client 4",  logo: "/images/clients/logo-lionz-04.png" },
-  { name: "Client 5",  logo: "/images/clients/logo-lionz-05.png" },
-  { name: "Client 6",  logo: "/images/clients/logo-lionz-06.png" },
-  { name: "Client 7",  logo: "/images/clients/logo-lionz-07.png" },
-  { name: "Client 8",  logo: "/images/clients/logo-lionz-08.png" },
-  { name: "Client 9",  logo: "/images/clients/logo-lionz-09.png" },
-  { name: "Client 10", logo: "/images/clients/logo-lionz-10.png" },
-  { name: "Client 11", logo: "/images/clients/logo-lionz-11.png" },
-  { name: "Client 12", logo: "/images/clients/logo-lionz-12.png" },
-  { name: "Client 13", logo: "/images/clients/logo-lionz-13.png" },
-  { name: "Client 14", logo: "/images/clients/logo-lionz-13.png" }
+  { name: "Client 1", logo: "/images/clients/logo-lionz-01.webp" },
+  { name: "Client 2", logo: "/images/clients/logo-lionz-02.webp" },
+  { name: "Client 3", logo: "/images/clients/logo-lionz-03.webp" },
+  { name: "Client 4", logo: "/images/clients/logo-lionz-04.webp" },
+  { name: "Client 5", logo: "/images/clients/logo-lionz-05.webp" },
+  { name: "Client 6", logo: "/images/clients/logo-lionz-06.webp" },
+  { name: "Client 7", logo: "/images/clients/logo-lionz-07.webp" },
+  { name: "Client 8", logo: "/images/clients/logo-lionz-08.webp" },
+  { name: "Client 9", logo: "/images/clients/logo-lionz-09.webp" },
+  { name: "Client 10", logo: "/images/clients/logo-lionz-10.webp" },
+  { name: "Client 11", logo: "/images/clients/logo-lionz-11.webp" },
+  { name: "Client 12", logo: "/images/clients/logo-lionz-12.webp" },
+  { name: "Client 13", logo: "/images/clients/logo-lionz-13.webp" },
+  { name: "Client 14", logo: "/images/clients/logo-lionz-13.webp" },
 ];
